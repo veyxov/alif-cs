788653298a5e241679180fff836fcc4589fecf37
Alex Wexouv <wexouv@gmail.com>
2021-08-04 00:03:09 +0500

    Added the rest

---
+++ lesson1.5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program {
+    // What if n / 60 > 24 * 60 ? :)
+
+    static int fullMinutes(int n) {
+        return n / 60;
+    }
+
+    static void Main() {
+        Console.WriteLine(fullMinutes(10985));
+    }
+}
---
+++ lesson1.6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program {
+
+    static int day(int n) {
+        // 1 2 3 4 5 6 7 8 9 10 ...
+        // 1 2 3 4 5 6 0 1 2  3 ...
+        return n % 7;
+    }
+
+    static void Main() {
+        Console.WriteLine(day(202));
+    }
+}
