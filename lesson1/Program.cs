48719736df2edd385c6816a11af6caaf5b978562
Shekhov Ismoil <veyxov@gmail.com>
2021-08-03 23:06:53 +0500

    Done first home work

---
+++ lesson1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program {
+    static double geometricMean(double a, double b) {
+        return Math.Sqrt(a * b);
+    }
+    static void Main() {
+        Console.WriteLine(geometricMean(16.8, 12.40));
+    }
+}
