cbadb29a31030b85a01682ca310a846dc95a5648
Alex Wexouv <wexouv@gmail.com>
2021-08-03 23:36:07 +0500

    Added the 3rd h/w

---
+++ lesson1.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program {
+    static double distance(double x1, double y1, double x2, double y2) {
+        double dx = x1 - x2;
+        double dy = y1 - y2;
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+    static void Main() {
+        Console.WriteLine(distance(-6.20, 5.2, 2.10, 9.8));
+    }
+}
