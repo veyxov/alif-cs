fd6707c34413be11f0f8c1e1c078d12376cee524
Shekhov Ismoil <veyxov@gmail.com>
2021-08-03 23:18:22 +0500

    Second home work done

---
+++ lesson1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program {
+    static void findLenOfLines(double a, double b, double c) {
+        // Math.Abs(A)
+        // |+A| = +A
+        // |-A| = +A
+
+        double ac = Math.Abs(a - c);
+        double bc = Math.Abs(b - c);
+        double sum = ac + bc;
+
+        Console.WriteLine($"ac  = {ac}");
+        Console.WriteLine($"bc  = {bc}");
+        Console.WriteLine($"sum = {sum}");
+    }
+    static void Main() {
+        findLenOfLines(1.4, -5.5, 0.6);
+    }
+}
