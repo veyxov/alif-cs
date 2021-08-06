89c14ffbc40abd74626054daeea1b2fbdf87519e
Alex Wexouv <wexouv@gmail.com>
2021-08-06 23:31:56 +0500

    Changed format of output

--- lesson2_05-08-2021/lesson2.3/Program.cs
+++ lesson2_05-08-2021/lesson2.3/Program.cs
@@ -2,21 +2,19 @@
 class Program {
 
     static string solve(int x) {
-        // [0 - 14], [15 - 35], [36 - 50], [50 - 100]
-        //    1          2          3          4
-        
-        int res = x switch {
-            >= 0  and <= 14  =>  1,
-            >= 15 and <= 35  =>  2,
-            >= 36 and <= 50  =>  3,
-            >= 50 and <= 100 =>  4,
-            _                => -1,
+
+        string res = x switch {
+            >= 0  and <= 14  =>  "[0  - 14]",
+            >= 15 and <= 35  =>  "[15 - 35]",
+            >= 36 and <= 50  =>  "[36 - 50]",
+            >= 50 and <= 100 =>  "[50 - 100]",
+            _                =>  "Error",
         };
-        return res == -1 ? "Incorrect range" : Convert.ToString(res);
+        return res;
     }
 
     static void Main() {
-        int x = 100;
+        int x = 2;
         Console.WriteLine(solve(x));
     }
 }
