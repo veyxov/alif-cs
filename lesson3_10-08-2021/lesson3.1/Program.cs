0b855502b862c5e46d084c0a759634ed69a3c55b
Alex Wexouv <wexouv@gmail.com>
2021-08-11 22:02:23 +0500

    Added 3rd lesson DZs

---
+++ lesson3_10-08-2021/lesson3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        double n = double.Parse(Console.WriteLine());
+        double rez = 0.0;
+
+        if (n > 1000) {
+            rez = (n * 5.0) / 100.0;
+        } else if (n > 500.0) {
+            rez = (n * 3.0) / 100.0;
+        }
+        Console.WriteLine(rez);
+    }
+}
---
+++ lesson3_10-08-2021/lesson3.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        int[] a = new int[4];
+        a[0] = int.Parse(Console.ReadLine());
+        a[1] = int.Parse(Console.ReadLine());
+        a[2] = int.Parse(Console.ReadLine());
+        a[3] = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i + 1 < 4; ++i) {
+            if (!(a[i] < a[i + 1])) {
+                // Output the min
+                Console.WriteLine(Math.Min(a[0], Math.Min(a[1], Math.Min(a[2], a[3]))));
+                return;
+            }
+        }
+        bool allTheSame = true;
+        for (int i = 0; i + 1 < 4; ++i) {
+            if (a[i] != a[i + 1]) {
+                allTheSame = false;
+                break;
+            }
+        }
+        if (allTheSame) {
+            Console.WriteLine(a[0] * a[1] * a[2] * a[3]);
+        } else {
+            Console.WriteLine("Numbers are increasing");
+        }
+    }
+}
