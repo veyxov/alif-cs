af652301a15d19c78d547906cf8cc4de2f124a7d
Shekhov Ismoil <veyxov@gmail.com>
2021-08-19 01:32:38 +0500

    Added another class example

---
+++ lesson6_17-08-2021/lesson6.3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+// This is an example of inheritance
+
+// Class for a *angle
+
+class Shape {
+    public double a;
+    public double b;
+}
+
+// The triangle class adds a new side
+class Triangle : Shape {
+    public double c;
+
+    public double GetPerimeter() {
+        return a + b + c;
+    }
+    // This class implements its 
+    // own Area-calculation method
+    public double GetArea() {
+        double s = GetPerimeter() / 2.0;
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    public Triangle() {
+
+    }
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+
+class Rectangle : Shape {
+    public double GetPerimeter() {
+        return 2 * (a + b);
+    }
+    public double GetArea() {
+        return a * b;
+    }
+    public Rectangle() {
+
+    }
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
+
+class Program {
+    static void Main() {
+        double a = 5.0 ;
+        double b = 5.0 ;
+        double c = 10.0;
+        Triangle tri = new Triangle(a, b, c);
+        Console.WriteLine(tri.GetArea());
+
+        Rectangle rec = new Rectangle(a, c);
+
+        Console.WriteLine(rec.GetArea());
+    }
+}
