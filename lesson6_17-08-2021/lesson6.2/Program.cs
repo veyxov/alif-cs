97cf6fb2cc2acee0800e9fdfd26a4ee76396038b
Alex Wexouv <wexouv@gmail.com>
2021-08-19 00:10:12 +0500

    Added another solution (simpler)

---
+++ lesson6_17-08-2021/lesson6.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Human {
+    public string name;
+    public int age;
+    public float weight;
+    public string work;
+
+    private bool hasJob;
+    private bool isAlive;
+    private bool isMale;
+    private int happiness;
+
+    public Human() {
+
+    }
+
+    public Human(string name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public GetInfo() {
+        Console.WriteLine($"name: {name} age: {age}");
+    }
+
+    public void Eat(string food) {
+        Console.WriteLine($"{name} ate {food}");
+        happiness++; // :)
+    }
+    public void ChangeName(string newName) {
+        this.name = newName;
+    }
+}
+
+class Program {
+    static void Main() {
+        Human me = new Human("Ismoil", 17);
+        me.GetInfo();
+    }
+}
