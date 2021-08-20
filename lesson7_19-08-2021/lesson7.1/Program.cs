e8737ae4377da01cb34cb1e15d818f3cab518b5e
Shekhov Ismoil <veyxov@gmail.com>
2021-08-20 16:29:56 +0500

    7th DZ

---
+++ lesson7_19-08-2021/lesson7.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Rectangle {
+    // Sides a and b of rectangle
+    private double side1, side2;
+
+    public double AreaCalculator() {
+        // Area of rectangle = a * b
+        return side1 * side2;
+    }
+
+    public double PerimeterCalculator() {
+        // Perimeter of rectangle = 2 * (a + b)
+        return 2 * (side1 + side2);
+    }
+
+    // This propriety returns the Area of the Rectangle
+    public double Area {
+        get {
+            return AreaCalculator();
+        }
+    }
+
+    // This propriety returns the Perimeter of the Rectangle
+    public double Perimeter {
+        get {
+            return PerimeterCalculator();
+        }
+    }
+
+    // Defauld constructor
+    public Rectangle() {
+
+    }
+    // Custom constructor
+    public Rectangle(double side1, double side2) {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+}
+class Program {
+    static void Main() {
+        Console.Write("Input the first side(a): ");
+        double.TryParse(Console.ReadLine(), out double a);
+
+        Console.Write("Input the second side(b): ");
+        double.TryParse(Console.ReadLine(), out double b);
+
+        // Instance of Rectangle Class
+        Rectangle rect = new Rectangle(a, b);
+
+        Console.WriteLine($"Perimeter: {rect.Perimeter}\nArea: {rect.Area}");
+    }
+}
---
+++ lesson7_19-08-2021/lesson7.2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Title {
+    private string title;
+    public void Show() {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Title: {title}");
+        Console.ResetColor();
+    }
+    public void Set(string title) {
+        this.title = title;
+    }
+}
+
+class Author {
+    private string author;
+    public void Show() {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Author: {author}");
+        Console.ResetColor();
+    }
+    public void Set(string author) {
+        this.author = author;
+    }
+}
+
+class Content {
+    private string content;
+    public void Show() {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Content: {content}");
+        Console.ResetColor();
+    }
+    public void Set(string content) {
+        this.content = content;
+    }
+}
+
+class Book {
+    public Title title;
+    public Author author;
+    public Content content;
+
+    // Default constructor
+    public Book() {
+        title = new Title();
+        author = new Author();
+        content = new Content();
+    }
+}
+
+
+class Program {
+    static void Main() {
+        Console.Write("Input book title: ");
+        string title = Console.ReadLine();
+
+        Console.Write("Input book author: ");
+        string author = Console.ReadLine();
+
+        Console.Write("Input book content (one line): ");
+        string content = Console.ReadLine();
+
+        Book book = new Book();
+        book.title.Set(title);
+        book.author.Set(author);
+        book.content.Set(content);
+
+        book.title.Show();
+        book.author.Show();
+        book.content.Show();
+    }
+}
