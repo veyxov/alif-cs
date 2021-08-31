24f618110615fb7163f751b2549edebedf423ba9
Alex Wexouv <wexouv@gmail.com>
2021-08-31 23:54:42 +0500

    Done

---
+++ lesson12_31-08-2021/lesson12.1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+interface IPlayable {
+    void Play();
+    void Pause();
+    void Stop();
+}
+
+interface IRecordable {
+    void Record();
+    void RecordPause();
+    void RecordStop();
+}
+
+// Wraper interface
+interface IRecAndPlayAble : IPlayable, IRecordable {  }
+
+class Player : IRecAndPlayAble {
+    public void Play() {
+        Console.WriteLine("Playing..");
+    }
+    public void Pause() {
+        Console.WriteLine("Pausing..");
+    }
+    public void Stop() {
+        Console.WriteLine("Stoping..");
+    }
+    public void Record() {
+        Console.WriteLine("Recoring ..");
+    }
+    public void RecordPause() {
+        Console.WriteLine("Pausing recording ..");
+    }
+    public void RecordStop() {
+        Console.WriteLine("Stopping recording ..");
+    }
+}
+
+// This class inherits only form one interface
+class Recorder : IRecordable {
+    public void Record() {
+        Console.WriteLine("Starting record !");
+    }
+    public void RecordPause() {
+        Console.WriteLine("Pausing record !");
+    }
+    public void RecordStop() {
+        Console.WriteLine("Stopping record!");
+    }
+}
+class Program {
+    static void Main() {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Player player = new Player();
+        player.Play();
+        player.Pause();
+        player.Stop();
+        player.Record();
+        player.RecordPause();
+        player.RecordStop();
+
+        Console.WriteLine();
+        // Recorder
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Recorder rec = new Recorder();
+        rec.Record();
+        rec.RecordPause();
+        rec.RecordStop();
+
+        Console.ResetColor();
+    }
+}
