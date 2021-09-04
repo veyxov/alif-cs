688391823cce0be363a55f6b274f89cf41b023b5
Alex Wexouv <wexouv@gmail.com>
2021-09-04 18:03:32 +0300

    Fixed file locations

--- lesson13_02-09-2021/lesson13.1/Program.cs
+++ lesson13_02-09-2021/lesson13.1/Program.cs
@@ -1,8 +1,23 @@
-﻿using System;
-
-class Program {
-        static void Main() {
-
-        }
+using System;
+class Worker {
+    public string name;
+    public int age;
+    public decimal salary;
+    
+    public void GetInfo() {
+        Console.WriteLine($"Name: {name}\nAge: {age}\n salary: {salary}");
     }
+    
+    public Worker () {}
+    public Worker (string name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+}
+class Program {
+  public static void Main() {
+      Woker emp = new Woker("Worker", 20, 2500);
+      emp.GetInfo();
+  }
 }
