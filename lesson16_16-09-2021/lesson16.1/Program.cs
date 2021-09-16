4f77979bfefd1204103266444335a6184497958b
Alex Wexouv <wexouv@gmail.com>
2021-09-16 22:16:31 +0500

    Created the structure of the project

---
+++ lesson16_16-09-2021/lesson16.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.SqlClient;
+
+class Account {
+    public int      Id          { get; set; }
+    public string   Acc         { get; set; }
+    public int      Is_Active   { get; set; }
+    public DateTime Created_At  { get; set; }
+    public DateTime Updated_At  { get; set; }
+}
+
+class Transaction { 
+    public int      Id           { get; set; }
+    public int      Account_Id   { get; set; }
+    public decimal  Amount       { get; set; }
+    public DateTime Created_At   { get; set; }
+}
+
+class Program {
+    static void CreateAccount() {
+
+    }
+    static void ShowAccounts() {
+
+    }
+    static void Transfer(string from, string to, decimal amount) {
+
+    }
+    static void Main() {
+
+    }
+}
