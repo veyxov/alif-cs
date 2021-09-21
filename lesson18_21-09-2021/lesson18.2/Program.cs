dd04026f17b1f6d782b45db1e88717ffdc08081f
Shekhov Ismoil <veyxov@gmail.com>
2021-09-21 23:18:17 +0500

    Done first part

---
+++ lesson18_21-09-2021/lesson18.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson18._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
