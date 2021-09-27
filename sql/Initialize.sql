51b227040e477c8c7eb995221d225456d48f4302
Shekhov Ismoil <veyxov@gmail.com>
2021-09-27 16:22:32 +0500

    Added is_admin and transaction type to sql database Accounts table

--- sql/Initialize.sql
+++ sql/Initialize.sql
@@ -4,16 +4,18 @@ USE AlifBank
 
 create TABLE Accounts (
     Id INT IDENTITY PRIMARY KEY NOT NULL,
-    Login nvarchar(10) NOT NULL,
-    Password nvarchar(200) NOT NULL,
-    LastName nvarchar(20) NOT NULL,
-    FirstName nvarchar(20) NOT NULL,
+    Login NVARCHAR(200) NOT NULL,
+    Password NVARCHAR(200) NOT NULL,
+    LastName NVARCHAR(200) NOT NULL,
+    FirstName NVARCHAR(200) NOT NULL,
     Age INT NOT NULL,
     Gender INT NOT NULL,
+    Is_Admin INT NOT NULL,
 )
 
 create TABLE Transactions(
     Id INT IDENTITY PRIMARY KEY NOT NULL,
     Account_Id INT REFERENCES Accounts(Id) NOT NULL,
-    Amount decimal(20, 2) not null,
+    Amount DECIMAL(100, 2) NOT NULL,
+    Type NVARCHAR(200) NOT NULL,
 )
