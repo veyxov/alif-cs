e0a8d79effe7f43cb48b08efd55199f04ab8b706
Alex Wexouv <wexouv@gmail.com>
2021-10-07 22:26:58 +0500

    Added project structure

---
+++ lesson20_07-10-2021/lesson20.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Client
+{
+    public int Id { get; set; }
+    public decimal Balance { get; set; }
+
+    public void Insert() {
+
+    }
+    public void Update() {
+
+    }
+    public void Delete() {
+
+    }
+    public void Select() {
+
+    }
+}
+
+class Program
+{
+    // The pseudo-database
+    static public List<Client> clients = new List<Client>();
+    static void Main()
+    {
+
+    }
+}
