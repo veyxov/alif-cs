0f3ce41d7b5c096b8ffc95775ca2505cc8984938
Alex Wexouv <wexouv@gmail.com>
2021-10-12 23:42:30 +0500

    Applied changes to previous lesson

--- lesson21_09-10-2021/lesson21.1/Program.cs
+++ lesson21_09-10-2021/lesson21.1/Program.cs
@@ -4,85 +4,95 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static Random rnd = new Random();
-
-    /* Constants used in the program */
-    const string chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
-
-    static int height = Console.WindowHeight;
+    // Console dimensions
     static int width = Console.WindowWidth;
+    static int height = Console.WindowHeight - 2;
 
-    static int minLen = 5;
-    static int maxLen = height;
+    // Colors
+    const ConsoleColor BaseColor = ConsoleColor.DarkGreen;
+    const ConsoleColor TailColor = ConsoleColor.Green;
+    const ConsoleColor HeadColor = ConsoleColor.White;
+    // Speeds
+    static int flowSpeed = 40;
+    const int Margin = 3;
 
-    const int flowSpeed = 50;
-    const int genSpeed = 100;
+    // Random number generator
+    static Random rnd = new Random();
+    private static object locker = new object();
 
-    /* Get a random int from chars array and convert it to char */
-    static char GetRandomChar() { return Convert.ToChar(chars[rnd.Next(chars.Length)]); }
+    //static char GetRandomChar() => Convert.ToChar(rnd.Next(255));
+    static char GetRandomChar() => Convert.ToChar(rnd.Next(65, 97));
 
-    static void ChColor(ConsoleColor color = ConsoleColor.White) => Console.ForegroundColor = color;
+    /* Draws a char to console with specific coordinates and color */
+    static async Task DrawChar(int x, int y, bool is_clear = false, ConsoleColor color = BaseColor)
+    {
+        // Lock the cursor position (not doing so introduces bugs)
+        lock (locker)
+        {
+            // Set the position and print random char
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
 
+            char res = ' ';
+            if (!is_clear) res = GetRandomChar();
+            Console.Write(res);
+        }
+    }
     static async Task DrawLine(int x, int y, int len)
     {
-        Console.SetCursorPosition(x, y);
+        for (int i = 0; i < len; ++i)
+        {
+            await Task.Delay(flowSpeed);
+            ConsoleColor curColor = BaseColor;
 
-        for (int i = 0; i < len; ++i) {
-            char curChar = GetRandomChar();
-            Console.SetCursorPosition(x, y + i);
+            // Index from the end
+            int j = len - i - 1;
+            if (j == 0) curColor = HeadColor;
+            else if (j == 1) curColor = TailColor;
 
-            // Write the head with white
-            ChColor(ConsoleColor.White);
-            Console.Write(curChar);
-
-            // Go one back and redraw with green
-            Console.SetCursorPosition(x, y + i - 1);
-            ChColor(ConsoleColor.Green);
-            Console.Write(curChar);
-            //
-            // Go two back and redraw with green
-            Console.SetCursorPosition(x, y + i - 2);
-            ChColor(ConsoleColor.DarkGreen);
-            Console.Write(curChar);
-
-            Thread.Sleep(flowSpeed);
+            await DrawChar(x, y + i, false, curColor);
+            //await DrawChar(x, y + i - 1, false, TailColor);
+            //await DrawChar(x, y + i - 2, false, BaseColor);
+        }
+        for (int i = -2; i < len; ++i)
+        {
+            await DrawChar(x, y + i, true);
+            await Task.Delay(flowSpeed);
         }
     }
-    static async Task CleanLine(int x) 
-    {
-        for (int i = 0; i <= height; ++i) {
-            Console.SetCursorPosition(x, i);
-            Console.Write(' ');
-            Thread.Sleep(flowSpeed - 10);
-        }
-    }
+
     static async Task Draw()
     {
-        int x = rnd.Next(2, width);
-        int y = 2;
-        int len = rnd.Next(minLen, Math.Min(maxLen, height - y + 1));
-        await Task.Run(() => DrawLine(x, y, len));
-        await Task.Run(() => CleanLine(x));
+        int x = rnd.Next(Margin, width - Margin);
+        int y = rnd.Next(2, height - Margin);
+        int len = rnd.Next(3, 20);
+
+        await DrawLine(x, y, len);
     }
 
+    static async Task DrawMatrix(int amount)
+    {
+        var tasks = new Task[amount];
+        for (int i = 0; i < amount; ++i)
+        {
+            tasks[i] = Task.Factory.StartNew(
+                    () => Draw()
+                    );
+        }
+        await Task.WhenAll(tasks);
+    }
+    /* Initialize the screen */
     static void Init()
     {
-        Console.Clear(); // First clear the screen
-        Console.CursorVisible = false; // Hide the cursor
+        Console.Clear();
+        Console.CursorVisible = false;
     }
-    static void Main()
+    static async Task Main()
     {
         Init();
-        // Console size
-        IO.Debug($"{width} x {height}");
-
-        Timer timer = new Timer(new TimerCallback(Refresh), null, 10000, 10000);
-
         while (true) {
-            var curTask = Task.Run(() => Draw());
-            Thread.Sleep(genSpeed);
+            await DrawMatrix(5);
+            Thread.Sleep(500);
         }
     }
-
-    static void Refresh(object obj) { Console.Clear(); }
 }
