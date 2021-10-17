4299b2a29d72134c6d814b2410b313ae22b17532
Shekhov Ismoil <veyxov@gmail.com>
2021-10-17 17:22:28 +0500

    Done

---
+++ lesson23_16-10-2021/lesson23.1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+//using System.ComponentModel.DataAnnotations.Schema;
+
+// The table columns
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public static class SqlHelper
+{
+    // The context to use the db
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=DZ;User ID=sa;Password=qwerty112!");
+        }
+    }
+
+    // Creates new entity
+    public static async Task Create(Student newUser)
+    {
+        using var db = new StudentContext();
+        await db.AddAsync(newUser); // Add 
+        await db.SaveChangesAsync(); // Save changes
+        IO.Debug($"Creted user {newUser.Name}");
+    }
+
+
+    public static async Task<Student> FindById(int id)
+    {
+        using var db = new StudentContext();
+
+        var curUser = await  db.Students.FindAsync(id); // Find user by id
+        if (curUser == null) throw new Exception($"User with id: {id} not found.");
+
+        return curUser;
+    }
+    public static async Task DeleteById(int id)
+    {
+        using var db = new StudentContext();
+        db.Remove(await FindById(id)); // Remove the user, if found
+
+        await db.SaveChangesAsync();
+        IO.Debug("User deleted");
+    }
+
+    public static async Task PrintAll()
+    {
+        using var db = new StudentContext();
+        var studentsList = await db.Students.ToListAsync(); // Get list of students
+
+        // Print all found users
+        foreach (var i in studentsList)
+            IO.Print($"Name: {i.Name} Age: {i.Age}", ConsoleColor.Green);
+    }
+
+    // Finds Student by id and changes it to the newStudent
+    public static async Task Update(int id, Student newSudent) {
+        using var db = new StudentContext();
+
+        var curUser = await FindById(id);
+        // Apply the changes to the student
+        curUser.Name = newSudent.Name;
+        curUser.Age = newSudent.Age;
+
+        // Apply the changes to the database
+        db.Update(curUser);
+        await db.SaveChangesAsync();
+        IO.Debug("User updated");
+    }
+}
+
+class Program
+{
+    static async Task Main()
+    {
+        // Create
+        await SqlHelper.Create(new Student() { Name = "Elon", Age = 17 });
+        await SqlHelper.Create(new Student() { Name = "Qrbon", Age = 25 });
+        await SqlHelper.Create(new Student() { Name = "Mark", Age = 23 });
+        await SqlHelper.Create(new Student() { Name = "Sayvali", Age = 15 });
+        await SqlHelper.PrintAll();
+
+        IO.Print("\n");
+
+        await SqlHelper.DeleteById(1);
+        await SqlHelper.Update(2, new Student() { Name = "MODIF", Age = 100 });
+        await SqlHelper.PrintAll();
+    }
+}
