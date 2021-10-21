45d1b7d0b3a959d2e1b2b262180062c88d6f8b2b
Shekhov Ismoil <veyxov@gmail.com>
2021-10-21 23:03:56 +0500

    Added: Add() and Print()

---
+++ lesson25_21-10-2021/lesson25.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using System;
+
+public class Student
+{
+    public Student() { }
+
+    public Student(int id, string name, int age)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    /* Adds new student to the table */
+    static async Task Add(Student student)
+    {
+        using (IDbConnection db = new SqlConnection(cnnString)) {
+            string insertQuery = "INSERT INTO [dbo].[Students] ([Name], [Age]) VALUES (@name, @age)";
+            await db.QueryAsync(insertQuery, new { student.Name, student.Age });
+        }
+    }
+    /* This method prints the students table */
+    static async Task Print()
+    {
+        using (IDbConnection db = new SqlConnection(cnnString)) {
+            string printQuery = "SELECT * FROM Students";
+            List<Student> list = (await db.QueryAsync<Student>(printQuery)).ToList();
+
+            foreach (var student in list)
+                Console.WriteLine($"Id: {student.Id}\tName: {student.Name}\tAge: {student.Age}");
+        }
+    }
+    public const string cnnString = "Server=localhost; Initial catalog=DZ; User=sa; password=qwerty112!";
+    static async Task Main()
+    {
+
+    }
+}
