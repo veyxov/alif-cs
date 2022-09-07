d57e0cd4f8b00851813558f7558a2de7359c3259
Shekhov Ismoil <veyxov@gmail.com>
2022-09-07 14:48:07 +0500

    move constants to different file

--- AlifBank/Program.cs
+++ AlifBank/Program.cs
@@ -2,13 +2,6 @@
 
 namespace MainApp
 {
-    /* Constants to use in windows */
-    static public class Constants
-    {
-        public const int MIN_POINTS = 11;
-        public const string SorryMessage = "You cannot create a credit";
-        public const string Congrats = "You can create a credit !";
-    }
     /* Mehthods that hold the logic of the program */
     static public class Program
     {
